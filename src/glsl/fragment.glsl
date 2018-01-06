c4fe7d4d9893d4b66e96155ecddcdabd1555ab37
Ryan Achten <ryanachten@gmail.com>
2018-01-06 15:55:29 +1300

    Add mouse shader test

--- src/glsl/fragment.glsl
+++ src/glsl/fragment.glsl
@@ -1,7 +1,20 @@
-uniform vec2 u_resolution;
-        uniform float u_time;
+// ** Gradient **
+// uniform vec2 u_resolution;
+//   uniform float u_time;
+//
+//   void main() {
+//       vec2 st = gl_FragCoord.xy/u_resolution.xy;
+//       gl_FragColor=vec4(st.x,st.y,0.0,1.0);
+//   }
 
-        void main() {
-            vec2 st = gl_FragCoord.xy/u_resolution.xy;
-            gl_FragColor=vec4(st.x,st.y,0.0,1.0);
-        }
+// ** Mouse test **
+uniform vec2 u_resolution;
+  uniform float u_time;
+  uniform vec2 u_mouse;
+  // uniform vec2 u_resolution;
+
+  void main() {
+      vec2 mouse = u_mouse.xy/u_resolution.xy;
+      vec2 st = gl_FragCoord.xy/u_resolution.xy;
+      gl_FragColor=vec4(mouse.x,mouse.y,0.0,1.0);
+  }
