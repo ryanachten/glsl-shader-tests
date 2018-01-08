21f98208171c8dcb48a0d1272db35c86ebd6bb85
Ryan Achten <ryanachten@gmail.com>
2018-01-08 16:54:01 +1300

    Add sin and cosine tests to algorithmic drawing

--- src/glsl/02_algorithmicDrawing.glsl
+++ src/glsl/02_algorithmicDrawing.glsl
@@ -41,16 +41,48 @@ float plot(vec2 st, float pct){
 
 
 // *** Step and Smooth step ***
+// void main() {
+//     vec2 st = gl_FragCoord.xy/u_resolution;
+//
+//     // Step will return 0.0 unless the value is over 0.5 -  in that case it will return 1.0
+//     // float y = step(0.5,st.x);
+//
+//     // Smooth interpolation between 0.1 and 0.9
+//     float y = smoothstep(0.1,0.9,st.x);
+//
+//     vec3 color = vec3(y);
+//
+//     float pct = plot(st,y);
+//     color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
+//
+//     gl_FragColor = vec4(color,1.0);
+// }
+
+// *** Sin and Cos ***
+
 void main() {
     vec2 st = gl_FragCoord.xy/u_resolution;
 
-    // Step will return 0.0 unless the value is over 0.5 -  in that case it will return 1.0
-    // float y = step(0.5,st.x);
+    //Bouncing ball
+    float y = abs(sin(u_time - st.x));
 
-    // Smooth interpolation between 0.1 and 0.9
-    // float y = smoothstep(0.1,0.9,st.x);
+    // Dolphin dives
+    // float y = sin(u_time - st.x);
 
-    float y = sin(u_time + st.x);
+    // Phases
+    // float y = fract(sin(u_time - st.x));
+
+    // Shrinking waves
+    // float y = sin(u_time * st.x);
+
+    // Square waves
+    // float y = ceil(sin(u_time * st.x)) + floor(sin(u_time * st.x));
+
+    // Shifts
+    // float y = sin(u_time + PI * st.x);
+
+    // Mod shifts
+    // float y = mod(u_time + st.x, 0.5);
 
     vec3 color = vec3(y);
 
